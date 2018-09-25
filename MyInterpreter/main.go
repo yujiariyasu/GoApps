1c2e7985b1b288aa5703cb76d532da2f8b3ace3b
yujiariyasu <yuji.ariyasu@gmail.com>
2018-09-26 08:51:46 +0900

    add main.go

---
+++ MyInterpreter/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yujiariyasu/GoApps/MyInterpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is Ariyasu's programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands...\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
