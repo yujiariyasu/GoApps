7661156d63474daec74af13647155261adcd8b66
yujiariyasu <yuji.ariyasu@gmail.com>
2018-10-15 10:25:15 +0900

    add Boolean Type and TestBooleanExpression

--- MyInterpreter/ast/ast.go
+++ MyInterpreter/ast/ast.go
@@ -63,6 +63,11 @@ type InfixExpression struct {
 	Right    Expression
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -145,3 +150,7 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+func (b Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b Boolean) ExpressionNode()      {}
+func (b Boolean) String() string       { return b.Token.Literal }
